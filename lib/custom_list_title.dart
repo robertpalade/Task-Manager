efdb7e75d2c662045c45ddbac8d7fa141d043f65
robertpalade <palade.robert99@gmail.com>
2023-04-05 09:32:19 +0300

    added favourite and delete swap

--- lib/custom_list_title.dart
+++ lib/custom_list_title.dart
@@ -1,15 +1,20 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class CustomListTile extends StatefulWidget {
   final String title;
   final String description;
   bool? isChecked;
+  // final Function(bool) onCheckboxChanged;
+  final Function() onFavourite;
+  final Function() onDelete;
 
   CustomListTile({
     required this.title,
     required this.description,
     required this.isChecked,
+    // required this.onCheckboxChanged,
+    required this.onFavourite,
+    required this.onDelete,
   });
 
   @override
@@ -20,6 +25,7 @@ class _CustomListTileState extends State<CustomListTile>
     with SingleTickerProviderStateMixin {
   late AnimationController _animationController;
   late Animation<double> _animation;
+  bool _isFavourited = false;
 
   @override
   void initState() {
@@ -37,57 +43,109 @@ class _CustomListTileState extends State<CustomListTile>
     super.dispose();
   }
 
+  void _onHorizontalDrag(DragEndDetails details) {
+    if (details.primaryVelocity! < 0) {
+      widget.onDelete();
+    } else if (details.primaryVelocity! > 0) {
+      setState(() {
+        _isFavourited = !_isFavourited;
+      });
+      widget.onFavourite();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
+      onHorizontalDragEnd: _onHorizontalDrag,
       onTap: () {
         setState(() {
           widget.isChecked = !widget.isChecked!;
         });
         _animationController.forward(from: 0);
       },
-      child: Row(
-        children: [
-          Checkbox(
-            value: widget.isChecked,
-            onChanged: (bool? value) {
-              setState(() {
-                widget.isChecked = value ?? false;
-              });
-            },
-          ),
-          SizedBox(width: 8),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  widget.title,
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                SizedBox(height: 4),
-                AnimatedBuilder(
-                  animation: _animation,
-                  builder: (context, child) {
-                    return Opacity(
-                      opacity: _animation.value,
-                      child: Text(
-                        widget.description,
-                        style: TextStyle(
-                          fontSize: 14,
-                          color: Colors.grey,
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ],
+      child: Dismissible(
+        key: UniqueKey(),
+        direction: DismissDirection.horizontal,
+        onDismissed: (direction) {
+          if (direction == DismissDirection.startToEnd) {
+            widget.onDelete();
+          } else if (direction == DismissDirection.endToStart) {
+            setState(() {
+              _isFavourited = !_isFavourited;
+            });
+            widget.onFavourite();
+          }
+        },
+        background: Container(
+          color: Colors.red,
+          child: Align(
+            alignment: Alignment.centerLeft,
+            child: Padding(
+              padding: EdgeInsets.only(left: 16.0),
+              child: Icon(
+                Icons.delete,
+                color: Colors.white,
+              ),
             ),
           ),
-        ],
+        ),
+        secondaryBackground: Container(
+          color: Colors.yellow,
+          child: Align(
+            alignment: Alignment.centerRight,
+            child: Padding(
+              padding: EdgeInsets.only(right: 16.0),
+              child: Icon(
+                _isFavourited ? Icons.favorite : Icons.favorite_border,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+        child: Row(
+          children: [
+            Checkbox(
+              value: widget.isChecked,
+              onChanged: (bool? value) {
+                setState(() {
+                  widget.isChecked = value ?? false;
+                });
+              },
+            ),
+            SizedBox(width: 8),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    widget.title,
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 4),
+                  AnimatedBuilder(
+                    animation: _animation,
+                    builder: (context, child) {
+                      return Opacity(
+                        opacity: _animation.value,
+                        child: Text(
+                          widget.description,
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -35,60 +35,70 @@ class _MyListViewState extends State<MyListView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Task Manager'),
-        ),
-        body: Column(
-          children: [
-            Expanded(
-              child: ListView.builder(
-                itemCount: myData.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return CustomListTile(title: myData[index], isChecked: false, description: "bla bla");
-                },
-              ),
-            ),
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(
-          tooltip: 'Add task',
-          onPressed: () {
-            showDialog(
-              context: context,
-              builder: (BuildContext context) {
-                final textController = TextEditingController();
-                return AlertDialog(
-                  title: const Text('ADd task'),
-                  content: TextFormField(
-                    controller: textController,
-                    decoration:
-                        const InputDecoration(hintText: 'Enter text here'),
-                  ),
-                  actions: <Widget>[
-                    ElevatedButton(
-                      child: const Text('Cancel'),
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                    ),
-                    ElevatedButton(
-                      child: const Text('OK'),
-                      onPressed: () {
-                        setState(() {
-                          myData.add(textController.text);
-                          print(myData);
-                        });
-                        Navigator.of(context).pop();
-                      },
-                    ),
-                  ],
+      appBar: AppBar(
+        title: const Text('Task Manager'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: myData.length,
+              itemBuilder: (BuildContext context, int index) {
+                return CustomListTile(
+                  title: myData[index],
+                  isChecked: false,
+                  description: "bla bla",
+                  // onCheckboxChanged: (bool) {},
+                  onFavourite: () {},
+                  onDelete: () {
+                    setState(() {
+                      myData.remove(index);
+                    });
+                  },
                 );
               },
-            );
-          },
-          child: const Icon(Icons.add),
-        ),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        tooltip: 'Add task',
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              final textController = TextEditingController();
+              return AlertDialog(
+                title: const Text('ADd task'),
+                content: TextFormField(
+                  controller: textController,
+                  decoration:
+                      const InputDecoration(hintText: 'Enter text here'),
+                ),
+                actions: <Widget>[
+                  ElevatedButton(
+                    child: const Text('Cancel'),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                  ElevatedButton(
+                    child: const Text('OK'),
+                    onPressed: () {
+                      setState(() {
+                        myData.add(textController.text);
+                        print(myData);
+                      });
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              );
+            },
+          );
+        },
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
-
